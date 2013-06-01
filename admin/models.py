d2797cd28dae7ad7fcf8509772fefbad33e3fa81
Paul Labedan <plabedan@gmail.com>
2013-06-01 11:47:11 +0200

    add __unicode__ methods to every model

--- admin/models.py
+++ admin/models.py
@@ -3,7 +3,13 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.name
+
 class Album(models.Model):
     name = models.CharField(max_length=200)
     artist = models.ForeignKey(Artist)
-    date = models.DateField('insert date')
+    date = models.DateField('insert date', auto_now=True)
+
+    def __unicode__(self):
+        return u'{} - {}'.format(self.artist.name, self.name)
